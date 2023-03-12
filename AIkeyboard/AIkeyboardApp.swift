2b34e9ee3a30c1e4a2a3ef5bfe37aae672778619
takuwann2gou <67136600+takuwann2gou@users.noreply.github.com>
2023-03-12 15:04:51 +0900

    Initial Commit

---
+++ AIkeyboard/AIkeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIkeyboardApp.swift
+//  AIkeyboard
+//
+//  Created by 上野恭明 on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct AIkeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
